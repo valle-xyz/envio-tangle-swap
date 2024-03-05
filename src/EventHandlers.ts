3a9923a996795dfd5cbffd1087ce77c0bac680d2
valleXYZ <valentin.seehausen@gmail.com>
2024-03-05 15:37:13 +0100

    add simple schema

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,22 +1,13 @@
 import {
-  GreeterContract_NewGreeting_loader,
-  GreeterContract_NewGreeting_handler,
-  GreeterContract_ClearGreeting_loader,
-  GreeterContract_ClearGreeting_handler,
+  FactoryContract_PoolCreated_handler,
+  FactoryContract_PoolCreated_loader
 } from "../generated/src/Handlers.gen";
 
-import { UserEntity } from "../generated/src/Types.gen";
+import { PoolEntity } from "../generated/src/Types.gen";
 
-/**
-Registers a loader that loads any values from your database that your
-NewGreeting event handler might need on the Greeter contract.
-*/
-GreeterContract_NewGreeting_loader(({ event, context }) => {
-  //The id for the "User" entity derived from params of the NewGreeting event
-  const userId = event.params.user;
-  //Try load in in a "User" entity with id of the user param on the
-  //NewGreeting event
-  context.User.load(userId);
+
+FactoryContract_PoolCreated_loader(({ event, context }) => {
+  context.Pool.load(event.params.pool, {});
 });
 
 /**
@@ -24,74 +15,38 @@ Registers a handler that handles any values from the
 NewGreeting event on the Greeter contract and index these values into
 the DB.
 */
-GreeterContract_NewGreeting_handler(({ event, context }) => {
-  //The id for the "User" entity
-  const userId = event.params.user;
-  //The greeting string that was added.
-  const latestGreeting = event.params.greeting;
-
-  //The optional User entity that may exist already at "userId"
-  //This value would be undefined in the case that it was not loaded in the
-  //loader function above OR in the case where it never existed in the db
-  const currentUserEntity = context.User.get(userId);
-
-  //Construct the userEntity that is to be set in the DB
-  const userEntity: UserEntity = currentUserEntity
-    ? //In the case there is an existing "User" entity, update its
-      //latestGreeting value, increment the numberOfGreetings and append latestGreeting
-      //to the array of greetings
-      {
-        id: userId,
-        latestGreeting,
-        numberOfGreetings: currentUserEntity.numberOfGreetings + 1,
-        greetings: [...currentUserEntity.greetings, latestGreeting],
-      }
-    : //In the case where there is no User entity at this id. Construct a new one with
-      //the current latest greeting, an initial number of greetings as "1" and an initial list
-      //of greetings with only the latest greeting.
-      {
-        id: userId,
-        latestGreeting,
-        numberOfGreetings: 1,
-        greetings: [latestGreeting],
-      };
-
-  //Set the User entity in the DB with the constructed values
-  context.User.set(userEntity);
-});
-
-/**
-Registers a loader that loads any values from your database that your
-ClearGreeting event handler might need on the Greeter contract.
-*/
-GreeterContract_ClearGreeting_loader(({ event, context }) => {
-  //The id for the "User" entity derived from params of the ClearGreeting event
-  const userId = event.params.user;
-  //Try load in in a "User" entity with id of the user param on the
-  //ClearGreeting event
-  context.User.load(userId);
-});
-
-/**
-Registers a handler that handles any values from the
-ClearGreeting event on the Greeter contract and index these values into
-the DB.
-*/
-GreeterContract_ClearGreeting_handler(({ event, context }) => {
-  //The id for the "User" entity derived from params of the ClearGreeting event
-  const userId = event.params.user;
-  //The optional User entity that may exist already at "userId"
-  //This value would be "undefined" in the case that it was not loaded in the
-  //loader function above OR in the case where it never existed in the db
-  const currentUserEntity = context.User.get(userId);
-
-  if (currentUserEntity) {
-    //Only make any changes in the case that there is an existing User
-    //Simply clear the latestGreeting by setting it to "" (empty string)
-    //and keep all the rest of the data the same
-    context.User.set({
-      ...currentUserEntity,
-      latestGreeting: "",
-    });
+FactoryContract_PoolCreated_handler(({ event, context }) => {
+  const poolEntity: PoolEntity = {
+    id: event.params.pool,
+    untrackedVolumeUSD: 0,
+    totalValueLockedUSDUntracked: 0,
+    sqrtPrice: 0n,
+    token0_id: event.params.token0,
+    volumeToken0: 0,
+    collectedFeesUSD: 0,
+    totalValueLockedUSD: 0,
+    liquidityProviderCount: 0n,
+    liquidity: 0n,
+    token0Price: 0,
+    token1Price: 0,
+    observationIndex: 0n,
+    collectedFeesToken1: 0,
+    txCount: 0n,
+    totalValueLockedToken1: 0,
+    token1_id: event.params.token1,
+    volumeUSD: 0,
+    feesUSD: 0,
+    feeTier: event.params.fee,
+    feeGrowthGlobal0X128: 0n,
+    feeGrowthGlobal1X128: 0n,
+    collectedFeesToken0: 0,
+    totalValueLockedToken0: 0,
+    tick: undefined,
+    totalValueLockedETH: 0,
+    createdAtBlockNumber: 0n,
+    volumeToken1: 0,
+    createdAtTimestamp: BigInt(event.blockTimestamp)
   }
+
+  context.Pool.set(poolEntity);
 });
