190297272ef9375b815161c8dc4a3206c81783b9
valleXYZ <valentin.seehausen@gmail.com>
2024-03-06 11:07:36 +0100

    retry

--- src/viem.ts
+++ src/viem.ts
@@ -2,7 +2,7 @@ import { createPublicClient, http } from 'viem'
 
 export const viemClient = createPublicClient({
   transport: http('https://archive.evm.shimmer.network/v1/chains/smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s/evm', {
-    retryCount: 6,
+    retryCount: 5,
     retryDelay: 250
   })
 })
